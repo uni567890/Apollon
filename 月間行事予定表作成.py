f74403f7d11ec80d7ec76740d342293b516f56d2
uni567890 <akauni567890@gmail.com>
2025-02-21 16:13:52 +0900

    Update 月間行事予定表作成.py
    時刻を文字列に

--- 月間行事予定表作成.py
+++ 月間行事予定表作成.py
@@ -36,12 +36,10 @@ def write_to_excel(csv_file_content, excel_file_content, year, month):
             weekday_jp = weekdays_jp[weekday_num]
 
             try: #時刻フォーマットエラーを捕捉
-                start_time = datetime.strptime(start_time_str, '%H:%M').strftime('%H:%M')
-                end_time = datetime.strptime(end_time_str, '%H:%M').strftime('%H:%M')
+                start_time = datetime.strptime(str(start_time_str), '%H:%M').strftime('%H:%M')
+                end_time = datetime.strptime(str(end_time_str), '%H:%M').strftime('%H:%M')
             except ValueError:
                 st.warning(f"時刻形式エラー: {start_time_str}または{end_time_str}. 時間は空白で出力します。")
-                st.warning(f"問題の開始時刻文字列: {start_time_str}") # 追加
-                st.warning(f"問題の終了時刻文字列: {end_time_str}")   # 追加
                 start_time = ""
                 end_time = ""
 
